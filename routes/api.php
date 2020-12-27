574721119c7d33a0c1940ec73e566a22984d77f7
yoseph <yosephoche@gmail.com>
2020-12-27 18:47:41 +0800

    finish all api for exist model

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Orion\Facades\Orion;
 
+use App\Http\Controllers\Api\AuthController;
+
 use App\Http\Controllers\Api\UserProfileController;
 use App\Http\Controllers\Api\PekerjaanController;
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\KlasisController;
+use App\Http\Controllers\Api\AnggotaKeluargaController;
+use App\Http\Controllers\Api\JemaatController;
+use App\Http\Controllers\Api\KeluargaController;
+use App\Http\Controllers\Api\MajelisController;
+use App\Http\Controllers\Api\WilayahController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +30,12 @@ use App\Http\Controllers\Api\AuthController;
 Route::group(['as' => 'api.'], function() {
     Orion::resource('profile', UserProfileController::class);
     Orion::resource('pekerjaan', PekerjaanController::class);
+    Orion::resource('klasis', KlasisController::class);
+    Orion::resource('wilayah', WilayahController::class);
+    Orion::resource('jemaat', JemaatController::class);
+    Orion::resource('keluarga', KeluargaController::class);
+    Orion::resource('anggota-keluarga', AnggotaKeluargaController::class);
+    Orion::resource('majelis', MajelisController::class);
 });
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
