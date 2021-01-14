840bd0d096d6f24868366ed8ef090f195d24fe94
yoseph <yosephoche@gmail.com>
2021-01-14 18:32:09 +0800

    handle update user ednpoint and tanggal lahir datetime format

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 use App\Models\User;
 use App\Models\UserProfile;
@@ -71,9 +72,35 @@ class UserController extends Controller
                 'is_sidi' => $request->is_sidi,
                 'jemaat_id' => $request->jemaat_id,
                 'klasis_id' => $klasis->id,
-                'wilayah_id' => $klasis->wilayah_id
+                'wilayah_id' => $klasis->wilayah_id,
+                'tanggal_lahir' => Carbon::parse($request->tanggal_lahir)->format('Y-m-d H:i:s')
             ]);
         }
         
+    }
+
+    public function performUpdate(Request $request, Model $user, array $attributes): void
+    {
+        $attributes['password'] = Hash::make($request->password);
+        $user->fill($attributes);
+        $user->save();
+
+        $jemaat = Jemaat::with('klasis')->where('id', $request->jemaat_id)->first();
+        $klasis = $jemaat->klasis;
+        
+        if ($user->save()) {
+            $profile = UserProfile::create([
+                'user_id' => $user->id,
+                'nama' => $user->name,
+                'jenis_kelamin' => $request->jenis_kelamin,
+                'alamat' => $request->alamat,
+                'is_baptis' => $request->is_baptis,
+                'is_sidi' => $request->is_sidi,
+                'jemaat_id' => $request->jemaat_id,
+                'klasis_id' => $klasis->id,
+                'wilayah_id' => $klasis->wilayah_id,
+                'tanggal_lahir' => Carbon::parse($request->tanggal_lahir)->format('Y-m-d H:i:s')
+            ]);
+        }
     }
 }
