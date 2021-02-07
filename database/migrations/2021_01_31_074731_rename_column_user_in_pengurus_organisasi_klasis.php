a0df10923710219c89245135a3c6a9ea6016288c
yoseph <yosephoche@gmail.com>
2021-02-07 18:40:12 +0800

    fix migrations file

--- database/migrations/2021_01_31_074731_rename_column_user_in_pengurus_organisasi_klasis.php
+++ database/migrations/2021_01_31_074731_rename_column_user_in_pengurus_organisasi_klasis.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameColumnOrganisasiWilayahIdInPengurusOrganisasiKlasis extends Migration
+class RenameColumnUserInPengurusOrganisasiKlasis  extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,9 @@ class RenameColumnOrganisasiWilayahIdInPengurusOrganisasiKlasis extends Migratio
     public function up()
     {
         Schema::table('pengurus_organisasi_klasis', function (Blueprint $table) {
-            $table->dropForeign('pengurus_organisasi_wilayahs_organisasi_wilayah_id_foreign');
-            $table->renameColumn('organisasi_wilayah_id', 'organisasi_klasis_id');
+
+            $table->dropForeign('pengurus_organisasi_wilayahs_user_foreign');
+            $table->renameColumn('user', 'user_id');
         });
     }
 
@@ -27,7 +28,7 @@ class RenameColumnOrganisasiWilayahIdInPengurusOrganisasiKlasis extends Migratio
     public function down()
     {
         Schema::table('pengurus_organisasi_klasis', function (Blueprint $table) {
-            $table->renameColumn('organisasi_klasis_id', 'organisasi_wilayah_id');
+            $table->renameColumn('user_id', 'user');
         });
     }
 }
