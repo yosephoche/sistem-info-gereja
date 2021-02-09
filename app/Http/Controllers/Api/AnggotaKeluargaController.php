bf519ee71f817aed945ccaaf5a8819e57ce643fc
yoseph <yosephoche@gmail.com>
2021-02-09 21:17:52 +0800

    get anggota keluarga by id keluarga

--- app/Http/Controllers/Api/AnggotaKeluargaController.php
+++ app/Http/Controllers/Api/AnggotaKeluargaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use Orion\Http\Controllers\Controller;
 use Orion\Concerns\DisableAuthorization;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Http\Request;
 
@@ -12,5 +14,22 @@ use App\Models\AnggotaKeluarga;
 class AnggotaKeluargaController extends Controller
 {
     use DisableAuthorization;
+
     protected $model = AnggotaKeluarga::class;
+
+    protected function alwaysIncludes() : array
+    {
+        return ['profile', 'keluarga'];
+    }
+
+    protected function buildIndexFetchQuery(Request $request, array $requestedRelations): Builder
+    {
+        $query = parent::buildIndexFetchQuery($request, $requestedRelations);
+
+        if ($request->exists('keluarga')) {
+            $query->where('keluarga_id', $request->keluarga);
+        };
+
+        return $query;
+    }
 }
