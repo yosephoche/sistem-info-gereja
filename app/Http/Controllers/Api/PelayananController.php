f50bc1b5ab4a84f9111f5b85b1fe1022137d6dc6
yoseph <yosephoche@gmail.com>
2021-02-15 16:58:04 +0800

    update pelayanan controller

--- app/Http/Controllers/Api/PelayananController.php
+++ app/Http/Controllers/Api/PelayananController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Orion\Http\Controllers\Controller;
 use Orion\Concerns\DisableAuthorization;
 use Illuminate\Http\Request;
